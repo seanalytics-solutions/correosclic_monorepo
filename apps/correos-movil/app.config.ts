a2eab5e27002540f1725a9ce4e17d614d960c6cc
Emmanuel <bayonaj368@gmail.com>
2025-12-04 18:42:32 -0600

    [app] new bundler id

--- apps/correos-movil/app.config.ts
+++ apps/correos-movil/app.config.ts
@@ -19,7 +19,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     },
     ios: {
       supportsTablet: true,
-      bundleIdentifier: "com.seanalytics.correosdemexico",
+      bundleIdentifier: "com.seanalytics.correosdemexicov2",
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
         CFBundleURLTypes: [
